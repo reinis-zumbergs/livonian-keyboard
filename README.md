39ff065b8bd0276852c07e31cf981b41b6adbdd4
Reinis Zumbergs <reinis.zumbergs@gmail.com>
2025-10-26 14:40:00 +0200

    Install and run prettier

--- README.md
+++ README.md
@@ -1,70 +1,74 @@
 # Livonian Keyboard Layout
-This Livonian keyboard layout is an expansion of the widely used Latvian (apostrophe) keyboard layout. With this layout apostrophe and backtick become dead keys for entering diacritics. The layout is designed to be used with the standard US keyboard layout.
 
+This Livonian keyboard layout is an expansion of the widely used Latvian (apostrophe) keyboard layout. With this layout apostrophe and backtick become dead keys for entering diacritics. The layout is designed to be used with the standard US keyboard layout.
 
 ## Layout
 
 ### Key output without dead key
+
     qwertyuiop
     asdfghjkl
     zxcvbnm
 
 ### Key output after apostrophe
-*Dash means that the key is not modified*
+
+_Dash means that the key is not modified_
 
     äüēŗţȳūīōǫ
     āšḑ-ģ-ȯķļ
     žöč--ņõ
 
 ### Key output after backtick
-*Dash means that the key is not modified*
+
+_Dash means that the key is not modified_
 
     ǟǖ-------ǭ
     ------ȱ--
     -ȫ----ȭ
 
 ### Table
-*Dash means that the output is not modified*
+
+_Dash means that the output is not modified_
 
 | Key | Output | After apostrophe | After backtick |
-| --- | --- | --- | --- |
-| ` |   | ` | ` |
-| ' |   | ' | ' |
-| q | q | ä | ǟ |
-| w | w | ü | ǖ |
-| e | e | ē | - |
-| r | r | ŗ | - |
-| t | t | ţ | - |
-| y | y | ȳ | - |
-| u | u | ū | - |
-| i | i | ī | - |
-| o | o | ō | - |
-| p | p | ǫ | ǭ |
-| a | a | ā | - |
-| s | s | š | - |
-| d | d | ḑ | - |
-| f | f | - | - |
-| g | g | ģ | - |
-| h | h | - | - |
-| j | j | ȯ | ȱ |
-| k | k | ķ | - |
-| l | l | ļ | - |
-| z | z | ž | - |
-| x | x | ö | ȫ |
-| c | c | č | - |
-| v | v | - | - |
-| b | b | - | - |
-| n | n | ņ | - |
-| m | m | õ | ȭ |
-
+| --- | ------ | ---------------- | -------------- |
+| `   |        | `                | `              |
+| '   |        | '                | '              |
+| q   | q      | ä                | ǟ              |
+| w   | w      | ü                | ǖ              |
+| e   | e      | ē                | -              |
+| r   | r      | ŗ                | -              |
+| t   | t      | ţ                | -              |
+| y   | y      | ȳ                | -              |
+| u   | u      | ū                | -              |
+| i   | i      | ī                | -              |
+| o   | o      | ō                | -              |
+| p   | p      | ǫ                | ǭ              |
+| a   | a      | ā                | -              |
+| s   | s      | š                | -              |
+| d   | d      | ḑ                | -              |
+| f   | f      | -                | -              |
+| g   | g      | ģ                | -              |
+| h   | h      | -                | -              |
+| j   | j      | ȯ                | ȱ              |
+| k   | k      | ķ                | -              |
+| l   | l      | ļ                | -              |
+| z   | z      | ž                | -              |
+| x   | x      | ö                | ȫ              |
+| c   | c      | č                | -              |
+| v   | v      | -                | -              |
+| b   | b      | -                | -              |
+| n   | n      | ņ                | -              |
+| m   | m      | õ                | ȭ              |
 
 ## Bookmarklet
+
 Bookmarklet is an easy, no-installation method of temporarily enabling the Livonian keyboard layout on (almost) any website. For instructions, see the [bookmarklet page](https://reinis-zumbergs.github.io/livonian-keyboard/bookmarklet/BOOKMARKLET.html).
 
-
 ## TODO
+
 - Bookmarklet: Show visual indicator when the layout is active
 
-
 ## License
+
 This project is licensed under the MIT License.
--- bookmarklet/livonian-keyboard.js
+++ bookmarklet/livonian-keyboard.js
@@ -1,73 +1,125 @@
 // This code should be loaded via a bookmarklet.
 // It will activate the Livonian keyboard on the current page.
 (function () {
+	// Check if the event handler is already added
+	if (window.livonianKeyboardActive) {
+		console.info("Livonian keyboard is already active");
+		return;
+	}
+	window.livonianKeyboardActive = true;
 
-    // Check if the event handler is already added
-    if (window.livonianKeyboardActive) {
-        console.info('Livonian keyboard is already active');
-        return;
-    }
-    window.livonianKeyboardActive = true;
+	const deadKeyMap = {
+		"'": {
+			q: "ä",
+			w: "ü",
+			e: "ē",
+			r: "ŗ",
+			t: "ţ",
+			y: "ȳ",
+			u: "ū",
+			i: "ī",
+			o: "ō",
+			p: "ǫ",
+			a: "ā",
+			s: "š",
+			d: "ḑ",
+			g: "ģ",
+			j: "ȯ",
+			k: "ķ",
+			l: "ļ",
+			z: "ž",
+			x: "ö",
+			c: "č",
+			n: "ņ",
+			m: "õ",
+			Q: "Ä",
+			W: "Ü",
+			E: "Ē",
+			R: "Ŗ",
+			T: "Ţ",
+			Y: "Ȳ",
+			U: "Ū",
+			I: "Ī",
+			O: "Ō",
+			P: "Ǫ",
+			A: "Ā",
+			S: "Š",
+			D: "Ḑ",
+			G: "Ģ",
+			J: "Ȯ",
+			K: "Ķ",
+			L: "Ļ",
+			Z: "Ž",
+			X: "Ö",
+			C: "Č",
+			N: "Ņ",
+			M: "Õ",
+		},
+		"`": {
+			q: "ǟ",
+			w: "ǖ",
+			j: "ȱ",
+			x: "ȫ",
+			m: "ȭ",
+			p: "ǭ",
+			Q: "Ǟ",
+			W: "Ǖ",
+			J: "Ȱ",
+			X: "Ȫ",
+			M: "Ȭ",
+			P: "Ǭ",
+		},
+	};
 
-    const deadKeyMap = {
-        "'": {
-            "q": "ä", "w": "ü", "e": "ē", "r": "ŗ", "t": "ţ", "y": "ȳ", "u": "ū", "i": "ī", "o": "ō", "p": "ǫ",
-            "a": "ā", "s": "š", "d": "ḑ", "g": "ģ", "j": "ȯ", "k": "ķ", "l": "ļ", "z": "ž", "x": "ö", "c": "č", "n": "ņ", "m": "õ",
-            "Q": "Ä", "W": "Ü", "E": "Ē", "R": "Ŗ", "T": "Ţ", "Y": "Ȳ", "U": "Ū", "I": "Ī", "O": "Ō", "P": "Ǫ",
-            "A": "Ā", "S": "Š", "D": "Ḑ", "G": "Ģ", "J": "Ȯ", "K": "Ķ", "L": "Ļ", "Z": "Ž", "X": "Ö", "C": "Č", "N": "Ņ", "M": "Õ",
-        },
-        "`": {
-            "q": "ǟ", "w": "ǖ", "j": "ȱ", "x": "ȫ", "m": "ȭ", "p": "ǭ",
-            "Q": "Ǟ", "W": "Ǖ", "J": "Ȱ", "X": "Ȫ", "M": "Ȭ", "P": "Ǭ",
-        }
-    };
+	let activeDeadKey = null;
 
-    let activeDeadKey = null;
+	document.addEventListener("keydown", function (event) {
+		// console.log('pressing', event.key);
+		// Ignore keydown events that are not printable characters
+		if (event.key.length !== 1) {
+			// console.log('Ignore non-printable key ' + event.key);
+			return;
+		}
 
-    document.addEventListener('keydown', function (event) {
-        // console.log('pressing', event.key);
-        // Ignore keydown events that are not printable characters
-        if (event.key.length !== 1) {
-            // console.log('Ignore non-printable key ' + event.key);
-            return;
-        }
+		if (activeDeadKey) {
+			// console.log('Active dead key ' + activeDeadKey);
+			if (deadKeyMap[activeDeadKey][event.key] !== undefined) {
+				const modifiedChar = deadKeyMap[activeDeadKey][event.key];
+				// console.log('Output mapped character ' + modifiedChar)
+				insertCharacter(modifiedChar);
+				// console.log('Prevent default ' + event.key);
+				event.preventDefault();
+			} else {
+				// console.log('Allow default ' + event.key);
+			}
 
-        if (activeDeadKey) {
-            // console.log('Active dead key ' + activeDeadKey);
-            if (deadKeyMap[activeDeadKey][event.key] !== undefined) {
-                const modifiedChar = deadKeyMap[activeDeadKey][event.key];
-                // console.log('Output mapped character ' + modifiedChar)
-                insertCharacter(modifiedChar);
-                // console.log('Prevent default ' + event.key);
-                event.preventDefault();
-            } else {
-                // console.log('Allow default ' + event.key);
-            }
+			// console.log('Clear active dead key');
+			activeDeadKey = null;
 
-            // console.log('Clear active dead key');
-            activeDeadKey = null;
+			return;
+		}
 
-            return;
-        }
+		if (deadKeyMap[event.key] !== undefined) {
+			// console.log('Set active dead key ' + event.key);
+			activeDeadKey = event.key;
+			event.preventDefault();
+		}
+	});
 
-        if (deadKeyMap[event.key] !== undefined) {
-            // console.log('Set active dead key ' + event.key);
-            activeDeadKey = event.key;
-            event.preventDefault();
-        }
-    });
+	console.info("Livonian keyboard activated");
 
-    console.info('Livonian keyboard activated');
-
-    function insertCharacter(char) {
-        const activeElement = document.activeElement;
-        if (activeElement && activeElement.value !== undefined) {
-            const start = activeElement.selectionStart;
-            const end = activeElement.selectionEnd;
-            activeElement.value = activeElement.value.substring(0, start) + char + activeElement.value.substring(end);
-            activeElement.setSelectionRange(start + 1, start + 1);
-        } else {
-            document.execCommand('insertText', false, char);
-        }
-    }
+	function insertCharacter(char) {
+		const activeElement = document.activeElement;
+		if (activeElement && activeElement.value !== undefined) {
+			const start = activeElement.selectionStart;
+			const end = activeElement.selectionEnd;
+			activeElement.value =
+				activeElement.value.substring(0, start) +
+				char +
+				activeElement.value.substring(end);
+			activeElement.setSelectionRange(start + 1, start + 1);
+		} else {
+			document.execCommand("insertText", false, char);
+		}
+	}
 })();
